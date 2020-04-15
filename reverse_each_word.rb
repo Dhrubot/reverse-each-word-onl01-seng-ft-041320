d321bae2ea8d7719011c8b6bfe680d53f27b35d9
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-15 22:19:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(str)
   reversed_arr = []
   arr = str.split
-  arr.map {|word| reversed_arr << word.reverse }
+  arr.collect {|word| reversed_arr << word.reverse }
   reversed_arr.join(" ")
 end
